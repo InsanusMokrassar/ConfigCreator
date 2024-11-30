f644f9e472b9dbb6bc6c502cc5d79e24558bc5b3
akkkihi <akkihiguy@gmail.com>
2024-11-30 14:07:51 +0300

    refactor after merge

--- features/schema_builder/src/jsMain/kotlin/ui/Main.kt
+++ features/schema_builder/src/jsMain/kotlin/ui/Main.kt
@@ -2,25 +2,21 @@ package dev.inmo.config_creator.features.schema_builder.client.ui
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import dev.inmo.config_creator.features.common.client.ui.stylesheet.ResetStyles
 import dev.inmo.config_creator.features.schema.common.models.MapSchemaItem
 import dev.inmo.config_creator.features.schema.common.models.Schema
-import dev.inmo.config_creator.features.schema.common.models.StringSchemaItem
 import dev.inmo.config_creator.features.schema_builder.client.ui.schema.SchemaDrawer
 import dev.inmo.config_creator.features.schema_builder.client.ui.schema.SchemaDrawerStyleSheet
 import dev.inmo.micro_utils.common.*
 import dev.inmo.micro_utils.coroutines.compose.enableStyleSheetsAggregator
 import dev.inmo.micro_utils.mime_types.KnownMimeTypes
-import dev.inmo.micro_utils.mime_types.MimeType
-import kotlinx.browser.document
-import kotlinx.browser.window
 import org.jetbrains.compose.web.renderComposable
-import org.w3c.dom.mediasource.MediaSource
 import org.w3c.dom.url.URL
 import org.w3c.files.Blob
 
 fun main() {
     renderComposable("root") {
-        enableStyleSheetsAggregator(setOf(SchemaDrawerStyleSheet))
+        enableStyleSheetsAggregator(setOf(SchemaDrawerStyleSheet, ResetStyles))
         val schemaState = remember { mutableStateOf<Schema>(Schema(MapSchemaItem(emptyList()))) }
         SchemaDrawer(
             schemaState.value,
