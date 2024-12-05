340678cd92b6f3ffec09047ef2299cc515b31f96
akkkihi <akkihiguy@gmail.com>
2024-12-05 05:58:55 +0300

    remove border

--- features/common/client/src/jsMain/kotlin/ui/stylesheet/BooleanStyleSheet.kt
+++ features/common/client/src/jsMain/kotlin/ui/stylesheet/BooleanStyleSheet.kt
@@ -7,12 +7,6 @@ object BooleanStyleSheet : StyleSheet() {
     val main by style {
         display(DisplayStyle.Flex)
         alignItems(AlignItems.Center)
-        border {
-            width(2.px)
-            style(LineStyle.Solid)
-            color(Color.white)
-        }
-        borderRadius(0.25.cssRem)
         gap(8.px)
         padding(4.px, 8.px)
     }
